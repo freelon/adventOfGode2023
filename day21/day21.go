de15ec75c5277e21214a40070ce4e11a83d76b6f
Johannes <freelon@gmx.net>
2023-12-22 22:27:18 +0100

    Day 21 part 2.

--- day21/day21.go
+++ day21/day21.go
@@ -2,6 +2,7 @@ package day21
 
 import (
 	"adventOfGode2023/util"
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -11,6 +12,12 @@ func Part1(input string) string {
 }
 
 func solve1(input string, remainingSteps int) string {
+	garden, start := parseGarden(input)
+	count := count(start, garden, remainingSteps)
+	return strconv.Itoa(count)
+}
+
+func parseGarden(input string) (map[Pos]rune, Pos) {
 	garden := make(map[Pos]rune)
 	var start Pos
 	for y, line := range strings.Split(input, "\n") {
@@ -22,8 +29,7 @@ func solve1(input string, remainingSteps int) string {
 			garden[Pos{x, y}] = r
 		}
 	}
-	count := count(start, garden, remainingSteps)
-	return strconv.Itoa(count)
+	return garden, start
 }
 
 type Progress struct {
@@ -32,9 +38,22 @@ type Progress struct {
 }
 
 func count(start Pos, garden map[Pos]rune, remainingSteps int) (reachableCount int) {
+	if remainingSteps < 0 {
+		return 0
+	}
+	visited := markFields(start, garden, remainingSteps)
+	for _, v := range visited {
+		if v%2 == remainingSteps%2 {
+			reachableCount++
+		}
+	}
+	return
+}
+
+func markFields(start Pos, garden map[Pos]rune, remainingSteps int) map[Pos]int {
 	var queue util.Queue[Progress]
 	queue.Enqueue(Progress{p: start, d: 0})
-	visited := make(map[Pos]bool)
+	visited := make(map[Pos]int)
 	for next, ok := queue.Dequeue(); ok; next, ok = queue.Dequeue() {
 		target, ok := garden[next.p]
 		if !ok {
@@ -49,16 +68,13 @@ func count(start Pos, garden map[Pos]rune, remainingSteps int) (reachableCount i
 		if next.d > remainingSteps {
 			continue
 		}
-		if next.d%2 == remainingSteps%2 {
-			reachableCount++
-		}
-		visited[next.p] = true
+		visited[next.p] = next.d
 
 		for _, neighbor := range next.p.neighbors() {
 			queue.Enqueue(Progress{neighbor, next.d + 1})
 		}
 	}
-	return
+	return visited
 }
 
 type Pos struct {
@@ -74,6 +90,127 @@ func (p Pos) neighbors() []Pos {
 	}
 }
 
-func Part2(_ string) string {
-	return ""
+func Part2(input string) string {
+	return solve2(input, 26501365)
+}
+
+func solve2(input string, remainingSteps int) string {
+	L := len(strings.Split(input, "\n")[0])
+	l := (L - 1) / 2
+	garden, startCenter := parseGarden(input)
+	center := count(startCenter, garden, remainingSteps)
+
+	startForWest := Pos{x: L - 1, y: startCenter.y}
+	w1 := count(startForWest, garden, 2*L)
+	w2 := count(startForWest, garden, 2*L+1)
+
+	startForEast := Pos{x: 0, y: startCenter.y}
+	e1 := count(startForEast, garden, 2*L)
+	e2 := count(startForEast, garden, 2*L+1)
+
+	startForNorth := Pos{x: startCenter.x, y: L - 1}
+	n1 := count(startForNorth, garden, 2*L)   // reach every second one
+	n2 := count(startForNorth, garden, 2*L+1) // reach every other second one
+
+	startForSouth := Pos{x: startCenter.x, y: 0}
+	s1 := count(startForSouth, garden, 2*L)
+	s2 := count(startForSouth, garden, 2*L+1)
+
+	startForNorthEast := Pos{x: 0, y: L - 1}
+	startForNorthWest := Pos{x: L - 1, y: L - 1}
+	startForSouthEast := Pos{x: 0, y: 0}
+	startForSouthWest := Pos{x: L - 1, y: 0}
+
+	ne1 := count(startForNorthEast, garden, remainingSteps-L-1)
+	ne2 := count(startForNorthEast, garden, remainingSteps-2*L-1)
+	nw1 := count(startForNorthWest, garden, remainingSteps-L-1)
+	nw2 := count(startForNorthWest, garden, remainingSteps-2*L-1)
+	se1 := count(startForSouthEast, garden, remainingSteps-L-1)
+	se2 := count(startForSouthEast, garden, remainingSteps-2*L-1)
+	sw1 := count(startForSouthWest, garden, remainingSteps-L-1)
+	sw2 := count(startForSouthWest, garden, remainingSteps-2*L-1)
+
+	N := max(0, (remainingSteps-(l+1))/L-1)
+	nLast := count(startForNorth, garden, remainingSteps-(l+1)-(N+1)*L)
+	nSecondLast := count(startForNorth, garden, remainingSteps-(l+1)-N*L)
+	sLast := count(startForSouth, garden, remainingSteps-(l+1)-(N+1)*L)
+	sSecondLast := count(startForSouth, garden, remainingSteps-(l+1)-N*L)
+	wLast := count(startForWest, garden, remainingSteps-(l+1)-(N+1)*L)
+	wSecondLast := count(startForWest, garden, remainingSteps-(l+1)-N*L)
+	eLast := count(startForEast, garden, remainingSteps-(l+1)-(N+1)*L)
+	eSecondLast := count(startForEast, garden, remainingSteps-(l+1)-N*L)
+	xFirstCount := N/2 + N%2
+	xSecondCount := N / 2
+
+	blocks := max(0, N-1)
+	blackCount := (blocks + blocks%2) * (blocks + blocks%2) / 4
+	whiteCount := (blocks*blocks+blocks)/2 - blackCount
+
+	neThirdLast := count(startForNorthEast, garden, remainingSteps-(N)*L-1)
+	neSecondLast := count(startForNorthEast, garden, remainingSteps-(N+1)*L-1)
+	neLast := count(startForNorthEast, garden, remainingSteps-(N+2)*L-1)
+	nwThirdLast := count(startForNorthWest, garden, remainingSteps-(N)*L-1)
+	nwSecondLast := count(startForNorthWest, garden, remainingSteps-(N+1)*L-1)
+	nwLast := count(startForNorthWest, garden, remainingSteps-(N+2)*L-1)
+	seThirdLast := count(startForSouthEast, garden, remainingSteps-(N)*L-1)
+	seSecondLast := count(startForSouthEast, garden, remainingSteps-(N+1)*L-1)
+	seLast := count(startForSouthEast, garden, remainingSteps-(N+2)*L-1)
+	swThirdLast := count(startForSouthWest, garden, remainingSteps-(N)*L-1)
+	swSecondLast := count(startForSouthWest, garden, remainingSteps-(N+1)*L-1)
+	swLast := count(startForSouthWest, garden, remainingSteps-(N+2)*L-1)
+
+	fmt.Printf("N = %d\n", N)
+	fmt.Printf("blocks = %d\n", blocks)
+	fmt.Printf("black = %d, white = %d\n", blackCount, whiteCount)
+
+	// TODOs
+	// [x] north south east west, two final blocks and the full blocks in between
+	// [x] NE quadrant: full blocks
+	// [ ] NE quadrant: N * neLast, (N-1)*neSecondLast
+
+	// [nx][neLast]
+	// ...
+	// [n2][ne2][ne1]...[ne2][neSecondLast][neLast]
+	// [n1][ne1][ne2]...[ne1][ne2][neSecondLast][neLast]
+	// [S ][e1 ][e2 ]...[e1 ][e2 ][eSecondLast ][eLast ]
+
+	all := center +
+		w1*xFirstCount +
+		w2*xSecondCount +
+		wSecondLast +
+		wLast +
+		e1*xFirstCount +
+		e2*xSecondCount +
+		eSecondLast +
+		eLast +
+		n1*xFirstCount +
+		n2*xSecondCount +
+		nSecondLast +
+		nLast +
+		s1*xFirstCount +
+		s2*xSecondCount +
+		sSecondLast +
+		sLast +
+		ne1*blackCount +
+		ne2*whiteCount +
+		neThirdLast*N +
+		neSecondLast*(N+1) +
+		neLast*(N+2) +
+		nw1*blackCount +
+		nw2*whiteCount +
+		nwThirdLast*N +
+		nwSecondLast*(N+1) +
+		nwLast*(N+2) +
+		se1*blackCount +
+		se2*whiteCount +
+		seThirdLast*N +
+		seSecondLast*(N+1) +
+		seLast*(N+2) +
+		sw1*blackCount +
+		sw2*whiteCount +
+		swThirdLast*N +
+		swSecondLast*(N+1) +
+		swLast*(N+2)
+	return strconv.Itoa(all)
+
 }
--- day21/day21_test.go
+++ day21/day21_test.go
@@ -2,6 +2,7 @@ package day21
 
 import (
 	"adventOfGode2023/util"
+	"strings"
 	"testing"
 )
 
@@ -21,6 +22,44 @@ func TestPart1(t *testing.T) {
 	util.Assert(t, 16, solve1(input, 6))
 }
 
-func TestPart2(t *testing.T) {
-	util.Assert(t, nil, Part2(input))
+func TestBlank10(t *testing.T) {
+	native := solve1(makeGrid(101), 10)
+	util.Assert(t, native, solve2(makeGrid(11), 10))
+}
+
+func TestBlank20(t *testing.T) {
+	native := solve1(makeGrid(101), 20)
+	util.Assert(t, native, solve2(makeGrid(11), 20))
+}
+
+func TestBlank30(t *testing.T) {
+	native := solve1(makeGrid(101), 30)
+	util.Assert(t, native, solve2(makeGrid(11), 30))
+}
+
+func TestBlank40(t *testing.T) {
+	native := solve1(makeGrid(101), 40)
+	util.Assert(t, native, solve2(makeGrid(11), 40))
+}
+
+func TestBlank50(t *testing.T) {
+	native := solve1(makeGrid(101), 50)
+	util.Assert(t, native, solve2(makeGrid(11), 50))
+}
+
+func makeGrid(L int) (result string) {
+	if L%2 == 0 {
+		panic("need uneven side length")
+	}
+	for i := 0; i < L; i++ {
+		s := strings.Repeat(".", L)
+		if i == ((L - 1) / 2) {
+			x := []rune(s)
+			x[(L-1)/2] = 'S'
+			s = string(x)
+		}
+		result = result + s + "\n"
+	}
+	result = strings.TrimSpace(result)
+	return
 }
